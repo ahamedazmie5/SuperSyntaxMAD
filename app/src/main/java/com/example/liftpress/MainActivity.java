ca0c0586afd24a8462c738c6ad867ce0064e8a55
ahamedazmie5 <it20132750@my.sliit.lk>
2021-09-22 03:37:06 +0530

    first_commit

---
+++ app/src/main/java/com/example/liftpress/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.liftpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button navigate;
+    private Button navigate2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigate=findViewById(R.id.mainbtn1);
+        navigate2=findViewById(R.id.mainbtn2);
+        navigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
+        navigate2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
