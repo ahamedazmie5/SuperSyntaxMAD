df1406f2280493e9bb317c115da026b1077067f1
Poojanifernando <86950971+Poojanifernando@users.noreply.github.com>
2021-09-26 16:04:10 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/liftpress/MainActivity.java
+++ app/src/main/java/com/example/liftpress/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 //anodya098
+//anodyacomments
 public class MainActivity extends AppCompatActivity {
     private Button navigate;
     private Button navigate2;
